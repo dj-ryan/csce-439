d643055715640bee2939bdc931d29a19270fb62e
ITS Student <its-student@unl.edu>
2022-02-14 10:43:04 -0600

    initial update

---
+++ shared/catkin_ws/devel/share/gennodejs/ros/balboa_core/msg/_index.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let balboaLL = require('./balboaLL.js');
+let balboaMotorSpeeds = require('./balboaMotorSpeeds.js');
+
+module.exports = {
+  balboaLL: balboaLL,
+  balboaMotorSpeeds: balboaMotorSpeeds,
+};
