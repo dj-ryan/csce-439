efc0c08d6fccecdb6f8248b5d44593f9c793f831
ITS Student <its-student@unl.edu>
2022-02-15 14:48:43 -0600

    update line endings

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
-# docker run --rm -v "//c/users/your-name/wherever-you-have-your-439-839-folder/shared:/home/shared" -p 5901:5901 -it ros
+# pip install serial serial-tool
 
+# py tcp_serial_redirect.py COM3 57600 
+
+# rosrun balboa serial
+
+
+
+# docker run --rm -v "//c/users/your-name/wherever-you-have-your-439-839-folder/shared:/home/shared" -p 5901:5901 -it ros
+# docker run --rm -v "//c/users/its-student/Desktop/csce-439/shared:/home/shared" -p 5901:5901 -it ros
 
